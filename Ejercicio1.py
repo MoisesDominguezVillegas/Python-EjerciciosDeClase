62d72036680f07ac41bb2fa9be97bf82d823a9a5
MoisesDominguezVillegas <druidax66x@gmail.com>
2024-10-14 17:29:50 +0200

    first commit

---
+++ Ejercicio1.py
@@ -0,0 +1,10 @@
+# PRIMER EJERCICIO
+
+nombre = input("Escribe tu nombre aqui: ")
+print(f"Hola {nombre}! Me alegro de conocerle")
+
+# SEGUNDO EJERCICIO
+
+nombre = input("Escribe tu nombre aqui: ")
+nombre_mayuscula = nombre.upper()
+print (nombre_mayuscula)
